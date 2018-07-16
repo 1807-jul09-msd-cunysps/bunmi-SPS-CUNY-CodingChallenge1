0c50c51ab3a7a51896a9f03e1304ceacff5e0c8c
Bunmi Alo <alobunmi@gmail.com>
2018-07-16 17:03:18 -0400

    Updated Test client

--- Palindrome/TestClient/Program.cs
+++ Palindrome/TestClient/Program.cs
@@ -8,8 +8,30 @@ namespace TestClient
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            string myStr = string.Empty;
+
+            myStr = "A nut for a jar of tuna";
+            Console.WriteLine("Is Palindrome {0} {1}", myStr, IsPalindrome(myStr));
+
+            myStr = "borrow or rob";
+            Console.WriteLine("Is Palindrome {0} {1}", myStr, IsPalindrome(myStr));
+
+            myStr = "343";
+            Console.WriteLine("Is Palindrome {0} {1}", myStr, IsPalindrome(myStr));
+
+            Console.ReadLine();
+
         }
-    }
+            public static bool IsPalindrome(string myStr)
+            {
+                char[] arr = myStr.ToCharArray();
+                Array.Reverse(arr);
+                
+                string backwards = new string(arr);
+                
+            return myStr == backwards;
+            }
+    }   
 }
